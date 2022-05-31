03086b2ce164e796422194ad206ba9f7a3531dcb
Fabian Jimenez <fabian.jmrls@gmail.com>
2022-05-31 12:17:50 -0500

    adding some files

--- base-root.sh
+++ base-root.sh
@@ -10,7 +10,7 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-pacman -S grub efibootmgr networkmanager bspwm sxhkd sudo rofi feh ranger kitty cava xorg xorg-xinit neovim dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq iptables-nft ipset firewalld acpid os-prober xf86-video-amdgpu
+pacman -S grub efibootmgr networkmanager bspwm sxhkd sudo rofi ranger kitty polkit cronie xorg xorg-xinit neovim dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq iptables-nft ipset firewalld acpid os-prober xf86-video-amdgpu
 
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=Arch
 
@@ -20,6 +20,7 @@ systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable avahi-daemon
 systemctl enable tlp
+systemctl enable cronie.service
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable firewalld
